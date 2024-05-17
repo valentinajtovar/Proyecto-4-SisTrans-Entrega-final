363f186b5f9f68d3b54206c87d25746ac8a14428
Valentina Jiménez <lv.jimenez@uniandes.edu.co>
2024-05-16 19:38:26 -0500

    commit

--- src/main/java/uniandes/edu/co/proyecto/modelo/Usuario.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Usuario.java
@@ -1,29 +1,13 @@
 package uniandes.edu.co.proyecto.modelo;
 
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 
 
-@Entity
-@Table(name = "USUARIO")
+@ToString
 public class Usuario {
-    @Id
+    
     private Integer idUsuario;
 
-    @ManyToOne
-    @JoinColumn(name ="tipo_documento", referencedColumnName = "tipoDocumento")
     private TipoDocumento tipoDocumento;
 
     private Integer numeroDocumento;
